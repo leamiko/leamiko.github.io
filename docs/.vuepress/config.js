3ea9528a6499673afc26f98e53591e14a2e6665c
linmingquan <linmingquan@szcangshi.com>
2019-12-17 09:40:35 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  base: "/",
+  title: "leamiko's blog",
+  description: "do it first, then make it better",
+  themeConfig: {
+    // 你的GitHub仓库，请正确填写
+    repo: "https://github.com/leamiko/leamiko.github.io.git",
+    // 自定义仓库链接文字。
+    repoLabel: "My GitHub",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "FirstBlog", link: "/blog/first-blog.md" }
+    ]
+  }
+};
